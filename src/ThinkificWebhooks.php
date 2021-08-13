a2a7ac2c009a5f7ba1606d313d7ea70379cccd92
Colin Longworth <colin.t.longworth@gmail.com>
2021-08-13 13:57:30 -0400

    Update ThinkificWebhooks.php

--- src/ThinkificWebhooks.php
+++ src/ThinkificWebhooks.php
@@ -9,43 +9,95 @@ use stdClass;
 class ThinkificWebhooks extends ThinkificResource
 {
     /**
+     * * IMPORTANT: Webhook endpoints must use V2 of the Thinkific API. Set version to 2 in the Client constructor
+     */
+
+    /**
      * Returns a list of webhooks currently set up on the account
      *
-     * IMPORTANT: Not currently operational as requires API V2 endpoint
      *
      * @param array $options
      * @return stdClass
      */
-    public function list($options = []) {
-
+    public function list(array $options = [])
+    {
         return $this->client->get('webhooks', $options);
+    }
+
+    /**
+     * Creates a webhook
+     *
+     * @param string $topic
+     * @param string $target_url
+     * @param array $options
+     * @return stdClass
+     */
+    public function create(string $topic, string $target_url, array $options = [])
+    {
+        return $this->client->post('webhooks', array_merge(['topic' => $topic, 'target_url' => $target_url], $options));
+    }
+
+    /**
+     * Get a webhook by ID
+     *
+     * @param string $id
+     * @return stdClass
+     */
+    public function get(string $id)
+    {
+        return $this->client->get("webhooks/$id");
+    }
+
+    /**
+     * Update a webhook by ID
+     *
+     * @param string $id
+     * @param array $options
+     * @return stdClass
+     */
+    public function update(string $id, array $options)
+    {
+        return $this->client->put("webhooks/$id", $options);
+    }
+
+    /**
+     * Delete a webhook by ID
+     *
+     * @param string $id
+     * @return stdClass
+     */
+    public function delete(string $id)
+    {
+        return $this->client->delete("webhooks/$id");
     }
 
     /**
      * Validates a webhook
      *
      * @see    https://developers.thinkific.com/api/api-documentation/
-     * @param  string $body JSON Body from Webhook
+     * @param string $body JSON Body from Webhook
      * @param  string|array $header
      * @return bool
      * @throws Exception
      */
-    public function validate($body, $header)
+    public function validate(string $body, $header)
     {
         /* We can accept an array or a string */
-        if(is_array($header)) {
-
-            $header = isset($header['HTTP_X_THINKIFIC_HMAC_SHA256']) ? $header['HTTP_X_THINKIFIC_HMAC_SHA256'] : false;
+        if (is_array($header)) {
+            $header = $header['HTTP_X_THINKIFIC_HMAC_SHA256'] ?? false;
         }
 
         /* We need the header for comparison */
-        if(!$header) return false;
+        if (!$header) {
+            return false;
+        }
 
         $calculated = hash_hmac('sha256', $body, $this->client->apiToken, false);
 
-        if($calculated === $header) return true;
+        if ($calculated === $header) {
+            return true;
+        }
 
         return false;
-
     }
 }
